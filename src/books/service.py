0626be6cf06ad64e2ecb8e5f772d8dffcd422a66
Ariful Islam Farhad <ariful.farhad@therapservices.net>
2026-04-23 07:37:18 +0600

    service file

--- src/books/service.py
+++ src/books/service.py
@@ -13,7 +13,8 @@ class BookService:
     async def get_book(self, book_uid: str, session: AsyncSession):
         statement = select(Book).where(Book.uid == book_uid)
         result = await session.exec(statement)
-        return result.first()
+        book = result.first()
+        return book if book is not None else None
 
     async def create_book(self, book_data: BookCreateModel, session: AsyncSession):
         book_data_dict = book_data.model_dump()
@@ -25,7 +26,22 @@ class BookService:
     async def update_book(
         self, book_uid: str, update_data: BookUpdateModel, session: AsyncSession
     ):
-        pass
+        book_to_update = self.get_book(book_uid, session)
+
+        if book_to_update is not None:
+            update_data_dict = update_data.model_dump()
+            for k, v in update_data_dict.items():
+                setattr(book_to_update, k, v)
+            await session.commit()
+            return book_to_update
+        else:
+            return None
 
     async def delete_book(self, book_uid: str, session: AsyncSession):
-        pass
+        book_to_delete = self.get_book(book_uid, session)
+
+        if book_to_delete is not None:
+            await session.delete(book_to_delete)
+            await session.commit()
+        else:
+            return None
