9fe4e8a3f4e92b9c0aee5ca7d69dcd7f727bf7ce
ariful-farhad <arifulfarhad300@gmail.com>
2026-04-30 15:36:47 +0600

    Add JWT configuration and implement access token creation

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import timedelta, datetime
+from src.config import Config
+import jwt
 
 password_context = CryptContext(schemes=["bcrypt"])
 
@@ -10,3 +13,11 @@ def generate_password_hash(password: str) -> str:
 
 def verify_password(password: str, hash: str) -> bool:
     return password_context.verify(password, hash)
+
+
+def create_access_token(user_data: dict, expiry: timedelta):
+
+    payload = {}
+    token = jwt.encode(
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
+    )
